07ba888974194e961bb924ffa15c94cac4637d65
Thanh Do <thanhdo.school@gmail.com>
2025-01-30 13:01:00 -0800

    Implement basic movement

--- player/player.gd
+++ player/player.gd
@@ -1,11 +1,59 @@
-extends Node2D
+extends CharacterBody2D
+signal hit
+
+@export var speed = 400
+const MAX_SPEED = 400
+const ACCELERATION_H = 100
+const GRAVITY = 1000
+const JUMP_POWER = -300
+var screen_size
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print("hello godot!")
+	screen_size = get_viewport_rect().size
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _physics_process(delta: float) -> void:
+	if Input.is_anything_pressed():
+		if Input.is_action_pressed("move_down"):
+			velocity.y += 1
+		if Input.is_action_pressed("jump") && is_on_floor():
+			velocity.y = JUMP_POWER
+	
+	# Make player fall
+	velocity.y = velocity.y + GRAVITY * delta
+	
+	# Get the next velocity.x value
+	var direction = Input.get_axis("move_left", "move_right") * speed
+	if direction * velocity.x < 0:
+		velocity.x = 0
+	else:
+		velocity.x = move_toward(velocity.x, direction, ACCELERATION_H * delta)
+		velocity.x = clamp(velocity.x, -MAX_SPEED, MAX_SPEED)
+	move_and_slide()
+	if velocity.length() > 0:
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+		
+	position += velocity * delta # Delta is the amount of time it took for the prev frame to complete
+	position = position.clamp(Vector2.ZERO, screen_size) # Clamp makes it so we can't leave the screen
+
+	if velocity.x != 0:
+		$AnimatedSprite2D.animation = "walk"
+		$AnimatedSprite2D.flip_v = false
+		# See the note below about the following boolean assignment.
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+
+
+func _on_body_entered(body: Node2D) -> void:
+	hide() # Player will disappear after being hit
+	hit.emit() # Emits a signal
+	$CollisionShape2D.set_deferred("disabled", true) # Waits to safely disable collision
+	
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
