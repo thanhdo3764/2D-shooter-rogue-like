728ccfb924c536d707fc40fa02bcc0ca01b42dde
Ryan <ryandanver@gmail.com>
2025-02-06 21:02:51 -0800

    begin working on animation

--- enemies/flying_boss/boss_1.gd
+++ enemies/flying_boss/boss_1.gd
@@ -2,12 +2,15 @@ extends StaticBody2D
 
 enum bossState {
 	FLY_IDLE,		# idle flying movement
-	FLY_RAMPAGE,	# flying movement for when health is low
 	ATTACK_SPREAD,	# emitting bullets in a pattern
-	ATTACK_BEAM		# da laser beam
+	ATTACK_BEAM,	# da laser beam
+	FLY_RAMPAGE,	# flying movement for when health is low
 }
 
 @onready var player = $"../Player"
+@onready var animation = $AnimationPlayer
+@onready var state_timer = $StateTimer
+
 var state = bossState.FLY_IDLE
 
 # Called when the node enters the scene tree for the first time.
@@ -17,7 +20,20 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	match state:
 		bossState.FLY_IDLE:
-			pass
+			if !animation.is_playing():
+				animation.play("Idle")
+				
+			# state change
+			if state_timer.is_stopped():
+				# swtich to a random attack state (equal chance)
+				if randf() > 0.5:
+					state = bossState.ATTACK_SPREAD
+				else:
+					state = bossState.ATTACK_BEAM
+				state_timer.start()
+				# TODO: position snaps back to origin when stopping animation, apply animation position?
+				animation.stop()
+	print(state)
 		
 #func get_player_dir() -> Vector2:
 	#return 
