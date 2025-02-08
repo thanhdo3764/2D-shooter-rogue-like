ff251dcfb8a969d4bc233af0091e9ac0b6f9ee96
PilotB024 <159487293+PilotB024@users.noreply.github.com>
2025-02-07 18:43:56 -0800

    Add Functional Main Menu
    Adds a main menu with a functioning button to start and quit the game. On my end, I set this as the main scene in Godot (the one that first plays upon start-up), and to avoid confusion I renamed main.gd and main.tscn to level.gd and level.tscn respectively. We'll need to make those same edits to the main branch for the main menu to work properly.

---
+++ 2D-shooter-rogue-like-main/enemies/flying_boss/boss_1.gd
@@ -0,0 +1,74 @@
+extends StaticBody2D
+
+enum bossState {
+	FLY_IDLE,		# idle flying movement
+	FLY_RAMPAGE,	# flying movement for when health is low
+	ATTACK_BULLET,	# emitting bullets in a pattern
+	ATTACK_BEAM,	# da laser beam
+}
+
+@export var fire_rate : float = 0.35
+@export var hp : int = 50
+
+@onready var player = $"../Player"
+@onready var animation = $AnimationPlayer
+@onready var state_timer = $StateTimer
+@onready var shoot_timer = $ShootTimer
+
+var state = bossState.FLY_IDLE
+
+func _ready() -> void:
+	# Might not need to set the wait time manually
+	shoot_timer.wait_time = fire_rate
+
+func _physics_process(delta: float) -> void:
+	match state:
+		bossState.FLY_IDLE:
+			if !animation.is_playing():
+				animation.play("Idle")
+				
+			# state change
+			# TODO: state change when hp is low
+			if state_timer.is_stopped():
+				# swtich to a random attack state (equal chance)
+				#if randf() > 0.5:
+					#state = bossState.ATTACK_BULLET
+				#else:
+					#state = bossState.ATTACK_BEAM
+				
+				# only switch to bullet attack for now
+				state = bossState.ATTACK_BULLET
+				state_timer.start()
+				# TODO: position snaps back to origin when stopping animation, might replace animplayer with tween
+				animation.stop()
+			
+		bossState.ATTACK_BULLET:
+			# start shooting
+			if shoot_timer.is_stopped():
+				shoot_timer.start()
+			
+			# state change
+			# TODO: state change when hp is low
+			if state_timer.is_stopped():
+				state = bossState.FLY_IDLE
+				state_timer.start()
+				shoot_timer.stop()
+				
+		bossState.ATTACK_BEAM:
+			pass # unimplemented
+			
+		bossState.FLY_RAMPAGE:
+			pass # unimplemented
+			
+	# TODO: take appropriate action when boss dies
+	if hp <= 0:
+		pass
+		
+func _on_shoot_timer_timeout() -> void:
+	if state == bossState.ATTACK_BULLET:
+		# TODO: create bullet and set direction, speed
+		print("BOSS SHOOTING AT: ", get_player_dir())
+
+# returns the normalized direction vector from the boss to the player
+func get_player_dir() -> Vector2:
+	return self.global_position.direction_to(player.global_position).normalized()
