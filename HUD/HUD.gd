3d8f12cc294a06e5054f3858b0496f758c4f625d
Morgan Martin <158347917+smoregans@users.noreply.github.com>
2025-02-07 21:57:11 -0800

    Added HUD and PLAYER folder updates for health/shield bars
    - Added a CanvasLayer and changed ProgressBars to TextureProgressBars inside of the HUD scene
    - Updated HUD.gd health_bar.gd and shield_bar.gd logic to fetch player health/shield values
    - Updated HUD.tscn to structure and position the health and shield bars on the screen
    - Updated player.gd to include health and shield variables
    - Don't forget to add the new assets from our Google Drive :)

--- HUD/HUD.gd
+++ HUD/HUD.gd
@@ -1,11 +1,25 @@
-extends Node
+extends Control
 
+@export var player: Node
+@onready var health_bar = $HealthBar
+@onready var shield_bar = $ShieldBar
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print("Hello Godot!")
+	if player == null:
+		# Auto-detect the player node
+		player = get_tree().get_first_node_in_group("player")
+	if player:
+		health_bar.player = player
+		shield_bar.player = player
+		# Initialize progress bars
+		health_bar.value = player.health
+		health_bar.max_value = player.max_health
+		shield_bar.value = player.shield
+		shield_bar.max_value = player.max_shield
+	else:
+		print("Player node not found!")
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _process(_delta) -> void:
+	if player:
+		health_bar.value = player.health
+		shield_bar.value = player.shield
