2a802e6385d9c8ed38ed73f99030c923d7386586
D925301 <davidhoang777@gmail.com>
2025-02-14 15:36:59 -0800

    updates and refactoring of enemy_knight
    refactored state management and collision shape updates.
    
    added player detection and proper movement

--- HUD/health_bar.gd
+++ HUD/health_bar.gd
@@ -1,11 +1,8 @@
-extends ProgressBar
+extends TextureProgressBar
 
+@export var player: Node
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	print("Health Bar is ready")
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func update_health():
+	if player:
+		# Set the progress bar value to the player's health
+		self.value = player.health
--- enemies/enemy_knight/enemy_knight.gd
+++ enemies/enemy_knight/enemy_knight.gd
@@ -2,13 +2,12 @@ extends CharacterBody2D
 
 
 const GRAVITY = 1000
-const SPEED = 1500
+const SPEED = 100
 
 enum State {sleep, wakeup, walk}
-@export var current_state: State
-@export var direction : Vector2 = Vector2.LEFT
-#TODO
-#@export var hp : int = TBD
+var current_state: State = State.sleep
+var player: Node2D = null # reference to the player
+#TODO @export var hp : int = TBD
 
 # reference to collision shapes
 @onready var sleep_collision = $SleepCollision
@@ -18,66 +17,86 @@ enum State {sleep, wakeup, walk}
 
 
 func _ready():
-	current_state = State.sleep
-	update_collision_shape()
+	change_state(State.sleep)
 	
-	#TODO detection signal
+	# detection signal when player enters the detection area
+	if not detection_area.body_entered.is_connected(_on_detection):
+		detection_area.body_entered.connect(_on_detection)
+	
+	# signal for when animations are finished
+	if not sprite.animation_finished.is_connected(_on_animation_finished):
+			sprite.animation_finished.connect(_on_animation_finished)
 
 func _physics_process(delta: float) -> void:
-	enemy_gravity(delta)
-
+	velocity.y += GRAVITY * delta
+	
 	match current_state:
 		State.sleep:
 			enemy_sleep(delta)
 		State.wakeup:
-			enemy_wakeup()
+			pass
 		State.walk:
 			enemy_walk(delta)
 	
 	move_and_slide() 
 
-func enemy_gravity(delta: float):
-	velocity.y += GRAVITY * delta
-	
+# state manager
+func change_state(new_state: State):
+	if current_state == new_state:
+		return  # prevent unnecessary transitions
+
+	#DEBUG 0 = sleep, 1 = wakeup, 2 = walk
+	print("current state: ", current_state, " transitioning to ", new_state)
+
+	current_state = new_state
+	update_collision_shape()
+
+	match new_state:
+		State.sleep:
+			sprite.play("sleep")
+			velocity.x = 0
+		State.wakeup:
+			sprite.play("wakeup")
+		State.walk:
+			sprite.play("walk")
+			
+### state behaviors ###
 func enemy_sleep(delta: float):
 	velocity.x = move_toward(velocity.x, 0, SPEED * delta)
-	if sprite.animation != "sleep":
-		sprite.play("sleep") #ensures its in the sleep animation
 	
 func enemy_wakeup():
-	if current_state != State.wakeup:
-		current_state = State.wakeup
-		sprite.play("wakeup") #switches into and plays wakeup animation
-	
-	if not sprite.animation_finished.is_connected(_on_animation_finished):
-		sprite.animation_finished.connect(_on_animation_finished)
+	if current_state == State.sleep:
+		change_state(State.wakeup)
 
 func enemy_walk(delta: float):
-	velocity.x = direction.x * SPEED * delta
-	if sprite.animation != "walk":
-		sprite.play("walk")
+	if player: # player's reference is obtained from _on_detection
+		var direction = (player.global_position - global_position).normalized()
+		var new_velocity_x = direction.x * SPEED
+		#check direciton to flip sprite
+		if velocity.x > 0:
+			sprite.flip_h = false # face right
+		elif velocity.x < 0:
+			sprite.flip_h = true # face left
+
 	
 # changes hit/collision box/shape based on the current state
 func update_collision_shape():
-	if current_state == State.sleep:
-		sleep_collision.disabled = false
-		sleep_collision.visible = true
-		awake_collision.disabled = true
-		awake_collision.visible = false
-	elif current_state == State.wakeup or current_state == State.walk:
-		sleep_collision.disabled = true
-		sleep_collision.visible= false
-		awake_collision.disabled = false
-		awake_collision.visible = true
+	sleep_collision.visible = current_state == State.sleep
+	sleep_collision.disabled = current_state != State.sleep
+	awake_collision.visible = current_state != State.sleep
+	awake_collision.disabled = current_state == State.sleep
 		
 func _on_animation_finished():
-	if sprite.animation == "wakeup":
-		current_state=State.walk
-		update_collision_shape()
+	#DEBUG
+	print("animation finished: ", sprite.get_animation())
+	
+	if current_state == State.wakeup:
+		change_state(State.walk)
 
-#TODO triggers the enemy to change state to wakeup when the player's collision is in the detection area
-#func _on_detection():
-	#if player and current_state == State.sleep:
-		#enemy_wakeup()
+#triggers the enemy to change state to wakeup when the player's collision is in the detection area
+func _on_detection(body):
+	if body.is_in_group("player") and current_state == State.sleep:
+		player = body # stores the player's reference
+		enemy_wakeup()
 		
 #TODO attacking functions
