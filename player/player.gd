397add5fa6617270eba1f93c221a9f12afc7a7fc
Ryan <ryandanver@gmail.com>
2025-02-21 15:57:38 -0800

    add coyote time and variable jump height

--- player/player.gd
+++ player/player.gd
@@ -8,15 +8,17 @@ signal hit
 @export var max_shield: int = 100
 
 @export var SPEED: int = 100
-@export var ACCELERATION_H: int = 300
+@export var ACCELERATION_H: int = 475
 @export var GRAVITY: int = 1000
-@export var JUMP_POWER: int = -300
-var screen_size: Vector2
-var raycast: RayCast2D
+@export var JUMP_POWER: int = -310
 
+@onready var coyote_timer: Timer = $CoyoteTimer
+@onready var jump_timer: Timer = $JumpTimer
+@onready var raycast: RayCast2D = $RayCast2D
+
+var screen_size: Vector2
 var WEAPON_LOAD = preload("res://weapons/Sniper.tscn")
 var weapon
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -24,20 +26,38 @@ func _ready() -> void:
 	add_child(weapon)
 	weapon.position = $Weapon_Spawn.position
 	screen_size = get_viewport_rect().size
-	raycast = $RayCast2D
 
 	add_to_group("player") # for the HUD
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	if Input.is_action_pressed("jump") && is_on_floor(): velocity.y = JUMP_POWER
+	# handle jumping
+	if Input.is_action_pressed("jump") and (is_on_floor() or coyote_timer.time_left > 0):
+		velocity.y = JUMP_POWER
+		# stop the coyote timer to prevent repeated jumps
+		if not coyote_timer.is_stopped():
+			coyote_timer.stop()
+			
+	# jump cutoff
+	if Input.is_action_just_released("jump"):
+		# if the key is released when we're still early into the jump (higher negative value), reduce the velocity to make the jump shorter
+		if velocity.y < JUMP_POWER / 1.5:
+			velocity.y = velocity.y / 2
+	
+	# apply gravity
 	try_fall_through_platform()
 	velocity.y = velocity.y + GRAVITY * delta # Make player fall
+	
+	# move the player
+	var previously_on_floor = is_on_floor()
 	apply_horizontal_movement(delta)
 	move_and_slide() # Make collision with the floor
-	try_walk_animation()
+	
+	# if the player has just fell off a platform, start the coyote timer
+	if previously_on_floor and not is_on_floor() and velocity.y >= 0:
+		coyote_timer.start()
+	
 	update_position(delta)
-
+	try_walk_animation()
 
 func update_position(delta:float) -> void:
 	position += velocity * delta # Delta is the amount of time it took for the prev frame to complete
@@ -64,11 +84,13 @@ func try_walk_animation() -> void:
 func apply_horizontal_movement(delta:float) -> void:
 	var direction = Input.get_axis("move_left", "move_right") * SPEED
 	# Keep moving in same direction if in the air and no input
-	if not is_on_floor() and direction == 0: direction = velocity.x
+	if not is_on_floor() and direction == 0:
+		direction = velocity.x
 	# Instantly change direction when on floor
-	elif direction * velocity.x < 0 and is_on_floor: direction = 0
+	elif direction * velocity.x < 0 and is_on_floor():
+		direction = 0
+		
 	velocity.x = move_toward(velocity.x, direction, ACCELERATION_H * delta)
-
 
 func _on_body_entered(body: Node2D) -> void:
 	hide() # Player will disappear after being hit
