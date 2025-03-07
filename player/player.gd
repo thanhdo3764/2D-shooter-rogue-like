e6f421cc4b98ec61c1a1bc7fd662d44d6dc02283
Thanh Do <thanhdo.school@gmail.com>
2025-03-07 12:49:30 -0800

    Refactor player to use finite states

--- player/player.gd
+++ player/player.gd
@@ -6,10 +6,10 @@ signal hit
 @export var SHIELD: int = 50
 @export var MAX_SHIELD: int = 100
 
-@export var SPEED: int = 100
-@export var ACCELERATION_H: int = 475
-@export var GRAVITY: int = 1000
-@export var JUMP_POWER: int = -310
+@export var SPEED: int = 250
+@export var ACCELERATION_H: int = 800
+@export var GRAVITY: int = 2500
+@export var JUMP_POWER: int = -700
 
 @onready var coyote_timer: Timer = $CoyoteTimer
 @onready var jump_timer: Timer = $JumpTimer
@@ -19,6 +19,15 @@ var screen_size: Vector2
 
 var WEAPON_LOAD
 var weapon
+
+enum PlayerState {
+	STANDING,
+	FALLING,
+	JUMPING,
+	RUNNING,
+}
+
+var STATE: PlayerState = PlayerState.STANDING
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -35,39 +44,100 @@ func _ready() -> void:
 
 	add_to_group("player") # for the HUD
 
+	
 func _physics_process(delta: float) -> void:
 	# handle jumping
-	if Input.is_action_pressed("jump") and (is_on_floor() or coyote_timer.time_left > 0):
-		velocity.y = JUMP_POWER
-		# stop the coyote timer to prevent repeated jumps
-		if not coyote_timer.is_stopped():
-			coyote_timer.stop()
-			
-	# jump cutoff
-	if Input.is_action_just_released("jump"):
-		# if the key is released when we're still early into the jump (higher negative value), reduce the velocity to make the jump shorter
-		if velocity.y < JUMP_POWER / 1.5:
-			velocity.y = velocity.y / 2
+	match STATE:
+		PlayerState.STANDING:
+			handle_standing(delta)
+		PlayerState.FALLING:
+			handle_falling(delta)
+		PlayerState.JUMPING:
+			handle_jumping(delta)
+		PlayerState.RUNNING:
+			handle_running(delta)
 	
 	# apply gravity
 	try_fall_through_platform()
-	velocity.y = velocity.y + GRAVITY * delta # Make player fall
+	velocity.y += GRAVITY * delta # Make player fall
 	
 	# move the player
 	var previously_on_floor = is_on_floor()
-	apply_horizontal_movement(delta)
 	move_and_slide() # Make collision with the floor
 	
 	# if the player has just fell off a platform, start the coyote timer
 	if previously_on_floor and not is_on_floor() and velocity.y >= 0:
 		coyote_timer.start()
 	
-	update_position(delta)
+	position = position.clamp(Vector2.ZERO, screen_size)
 	try_walk_animation()
 
-func update_position(delta:float) -> void:
-	position += velocity * delta # Delta is the amount of time it took for the prev frame to complete
-	position = position.clamp(Vector2.ZERO, screen_size) # Clamp makes it so we can't leave the screen
+
+func handle_standing(delta: float) -> void:
+	if not is_on_floor():
+		STATE = PlayerState.FALLING
+		return
+	
+	var direction = Input.get_axis("move_left", "move_right") * SPEED
+	if direction != 0:
+		STATE = PlayerState.RUNNING
+		velocity.x = move_toward(velocity.x, direction, ACCELERATION_H*delta)
+	elif Input.is_action_pressed("jump"):
+		STATE = PlayerState.JUMPING
+		velocity.y = JUMP_POWER
+
+
+func handle_air_horizontal_input(delta: float) -> void:
+	var direction = Input.get_axis("move_left", "move_right") * SPEED
+	
+	if direction == 0:
+		direction = velocity.x
+
+	velocity.x = move_toward(velocity.x, direction, ACCELERATION_H * delta)
+
+		
+func handle_falling(delta: float) -> void:
+	if is_on_floor():
+		STATE = PlayerState.STANDING if velocity.x == 0 else PlayerState.RUNNING
+	else:
+		handle_air_horizontal_input(delta)
+	
+	if Input.is_action_pressed("jump") and coyote_timer.time_left > 0:
+		STATE = PlayerState.JUMPING
+		velocity.y = JUMP_POWER
+		# stop the coyote timer to prevent repeated jumps
+		if not coyote_timer.is_stopped():
+			coyote_timer.stop()
+
+
+func handle_jumping(delta: float) -> void:
+	if is_on_floor():
+		STATE = PlayerState.STANDING if velocity.x == 0 else PlayerState.RUNNING
+	elif velocity.y > 0:
+		STATE = PlayerState.FALLING
+	else:
+		if Input.is_action_just_released("jump"):
+			if velocity.y < JUMP_POWER / 1.5:
+				velocity.y = velocity.y / 2
+
+		handle_air_horizontal_input(delta)
+	
+
+func handle_running(delta: float) -> void:
+	if velocity.x == 0:
+		STATE = PlayerState.STANDING
+	if velocity.y > 0:
+		STATE = PlayerState.FALLING
+	elif Input.is_action_pressed("jump"):
+		STATE = PlayerState.JUMPING
+		velocity.y = JUMP_POWER
+	else:
+		var direction = Input.get_axis("move_left", "move_right") * SPEED
+		
+		if direction * velocity.x < 0: # Change directions quicker
+			velocity.x = move_toward(velocity.x, direction, 4 * ACCELERATION_H * delta)
+		else:
+			velocity.x = move_toward(velocity.x, direction, ACCELERATION_H * delta)
 
 
 func try_fall_through_platform() -> void:
@@ -86,17 +156,6 @@ func try_walk_animation() -> void:
 	
 	$AnimatedSprite2D.flip_h = (get_global_mouse_position() - global_position).x < 0
 		
-		
-func apply_horizontal_movement(delta:float) -> void:
-	var direction = Input.get_axis("move_left", "move_right") * SPEED
-	# Keep moving in same direction if in the air and no input
-	if not is_on_floor() and direction == 0:
-		direction = velocity.x
-	# Instantly change direction when on floor
-	elif direction * velocity.x < 0 and is_on_floor():
-		direction = 0
-		
-	velocity.x = move_toward(velocity.x, direction, ACCELERATION_H * delta)
 
 func _on_body_entered(body: Node2D) -> void:
 	hide() # Player will disappear after being hit
