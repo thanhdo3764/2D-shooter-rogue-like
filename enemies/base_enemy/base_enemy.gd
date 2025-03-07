9ee3b8aaa37663ea88766600cb0da83775501dfb
D925301 <davidhoang777@gmail.com>
2025-03-07 14:52:59 -0800

    base_enemy class
    - added a base class for enemies that have shared functions
    - changed enemy_knight collision boxes to be rectangles from circles to be consistent with other collision boxes

---
+++ enemies/base_enemy/base_enemy.gd
@@ -0,0 +1,32 @@
+extends CharacterBody2D
+class_name base_enemy
+
+const GRAVITY = 1000
+@export var SPEED: int = 100
+@export var hp: int = 10 
+
+var player: Node2D = null
+var DEFAULT_SCALE_X: float = 1.0
+var flipped: bool = false
+
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+func _ready():
+	DEFAULT_SCALE_X = scale.x
+
+func apply_gravity(delta:float):
+	velocity.y += GRAVITY * delta
+
+func move_towards_player():
+	if player:
+		var direction = (player.global_position - global_position).normalized()
+		var new_velocity_x = direction.x * SPEED
+
+		if velocity.x < 0 and scale.x > 0:
+			scale.x = -DEFAULT_SCALE_X
+			flipped = !flipped
+		if velocity.x > 0 and scale.x < 0:
+			scale.x = DEFAULT_SCALE_X
+			flipped = !flipped
+			
+		velocity.x = new_velocity_x
