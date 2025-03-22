5013977dd7ae7a21be4fa00532075046c074c3fc
PilotB024 <159487293+PilotB024@users.noreply.github.com>
2025-03-21 21:08:26 -0700

    Add First Modifier
    Adds a modifier item for purchase in the shop. You pay $1000 to double the rate at which you earn money.

--- player/player.gd
+++ player/player.gd
@@ -18,9 +18,6 @@ signal hit
 
 var screen_size: Vector2
 
-var WEAPON_LOAD
-var weapon
-
 enum PlayerState {
 	STANDING,
 	FALLING,
@@ -30,8 +27,13 @@ enum PlayerState {
 
 var STATE: PlayerState = PlayerState.STANDING
 
+var multiplier = 1
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	var WEAPON_LOAD = preload("res://weapons/Pistol.tscn")
+	var weapon
+	
 	if EquipItems.weapon == 1:
 		WEAPON_LOAD = preload("res://weapons/Pistol.tscn")
 		
@@ -44,8 +46,6 @@ func _ready() -> void:
 	screen_size = get_viewport_rect().size
 
 	add_to_group("player") # for the HUD
-	print("Player has $" + str(EquipItems._get_bank()) + " in their Bank.")
-
 	
 func _physics_process(delta: float) -> void:
 	# handle jumping
@@ -178,8 +178,10 @@ func start(pos):
 	$CollisionShape2D.disabled = false
 
 func _on_money_timer_timeout() -> void:
+	if EquipItems.modifier == 1:
+		multiplier = 2
 	if HEALTH > 0:
-		EquipItems.money += 5
+		EquipItems.money += (5 * multiplier)
 		print(EquipItems.money)
 		
 func _on_death() -> void:
