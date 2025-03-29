4c87053de54251fdd812e37970830b6b198cd9cd
PilotB024 <159487293+PilotB024@users.noreply.github.com>
2025-03-28 20:01:21 -0700

    Change Scoring
    Updated the score tag on the HUD to track the money you earned.
    Note: I meant to do a PR for the new modifiers I added but accidentally committed them directly to Main instead.

--- HUD/score.gd
+++ HUD/score.gd
@@ -8,11 +8,11 @@ var latest_score: int
 func _ready() -> void:
 	if player:
 		latest_score = player.SCORE
-		score_label.text = "Score: " + str(latest_score)
+		score_label.text = "Earned: $" + str(latest_score)
 	else:
 		print_debug("Player not found")
 
 func _process(delta: float) -> void:
 	if player and player.SCORE != latest_score:
 		latest_score = player.SCORE
-		score_label.text = "Score: " + str(latest_score)
+		score_label.text = "Earned: $" + str(latest_score)
