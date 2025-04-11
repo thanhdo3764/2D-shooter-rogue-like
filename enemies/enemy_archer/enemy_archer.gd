5fc87464b8eb572809e94507115f5a9612b329de
D925301 <davidhoang777@gmail.com>
2025-04-11 13:13:36 -0700

    Update enemy_archer.gd
    added attacking functions

--- enemies/enemy_archer/enemy_archer.gd
+++ enemies/enemy_archer/enemy_archer.gd
@@ -1,10 +1,13 @@
 extends base_enemy
 
-enum State {idle, walk, aim, hit, attack, dead}
+enum State {idle, walk, aim, attack, dead}
 var current_state: State = State.idle
 
 @export var arrow: PackedScene
-var attack_cooldown: float = 2.0
+var attack_cooldown: float = 1
+var can_attack: bool = true
+var player_in_range: bool = false
+
 @onready var attack_timer: Timer = $AttackTimer
 @onready var detection_area: Area2D = $DetectionArea
 
@@ -13,20 +16,37 @@ func _ready():
 	super()
 	change_state(State.idle)
 	
-	if detection_area and not detection_area.body_entered.is_connected(_on_detection):
+	# player enters detection
+	if not detection_area.body_entered.is_connected(_on_detection):
 		detection_area.body_entered.connect(_on_detection)
 	
-	#if not attack_timer.timeout.is_connected(_on_attack_timer_timeout):
-		#attack_timer.timeout.connect(_on_attack_timer_timeout)
-
+	# player leaves detection
+	if not detection_area.body_exited.is_connected(_on_detection):
+		detection_area.body_exited.connect(_on_detection_exit)
+	
+	if not attack_timer.timeout.is_connected(_on_attack_timer_timeout):
+		attack_timer.timeout.connect(_on_attack_timer_timeout)
+		
+	if not sprite.animation_finished.is_connected(_on_animation_finished):
+		sprite.animation_finished.connect(_on_animation_finished)
+	
 func _physics_process(delta: float) -> void:
 	apply_gravity(delta)
 	
 	match current_state:
 		State.idle:
-			archer_idle(delta)
+			if player_in_range and can_attack:
+				change_state(State.aim)
 		State.walk:
 			move_towards_player()
+			if player_in_range and can_attack:
+				change_state(State.aim)
+		State.aim:
+			velocity.x = 0
+			if can_attack:
+				change_state(State.attack)
+		State.attack:
+			velocity.x = 0
 
 	move_and_slide() 
 
@@ -40,39 +60,57 @@ func change_state(new_state:State):
 			sprite.play("idle")
 		State.walk:
 			sprite.play("walk")
-		State.hit:
-			sprite.play("hit")
+		State.aim:
+			sprite.play("idle")
 		State.attack:
 			sprite.play("attack")
 		State.dead:
 			sprite.play("death")
-	
-func archer_idle(delta: float):
-	pass
-	
-func _on_attack_timer_timeout():
-	if current_state == State.aim:
-		change_state(State.attack)
 
-#func archer_shoot(delta: float):
-	#shoot_arrow()
-	#shoot_timer.start(attack_cooldown)
-	#change(State.aim)
+func _on_animation_finished():
+	if current_state == State.attack:
+		shoot_arrow()
+		can_attack = false
+		attack_timer.start(attack_cooldown)
+
+		if player_in_range:
+			change_state(State.aim)
+		else:
+			change_state(State.walk)
+
+func _on_attack_timer_timeout():
+	can_attack = true
+	if player and current_state != State.attack and detection_area.has_overlapping_bodies():
+		change_state(State.aim)
+	else:
+		change_state(State.walk)
 	
 func shoot_arrow() -> void:
 	var arrow = arrow.instantiate()
 	arrow.global_position = self.global_position
 	arrow.set_target(player.global_position)
 	get_tree().current_scene.add_child(arrow)
-	arrow.connect("hit_arrow", player._on_bullet_hit())
+	
+	# uses _on_bullet_hit and in turn it's damage as well
+	arrow.connect("hit_arrow", Callable(player, "_on_bullet_hit"))
 
+func _on_detection(body):
+	if body.is_in_group("player"):
+		player = body
+		player_in_range = true
+		
+		if can_attack:
+			change_state(State.aim)
+		else:
+			change_state(State.walk)
+
+func _on_detection_exit(body):
+	if body == player:
+		player_in_range = false
+		change_state(State.walk)
+		
 func on_death():
 	if current_state == State.dead:
 		return
 	print("Enemy archer died")
 	change_state(State.dead)
-
-func _on_detection(body):
-	if body.is_in_group("player") and current_state == State.idle:
-		player = body
-		change_state(State.walk)
